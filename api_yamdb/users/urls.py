bf39ca2d12f2d5ab40ab95c2fb88a9b59af33311
Антон Дунайкин <for_letter@list.ru>
2023-04-14 22:53:15 +0300

    часть работы users

---
+++ api_yamdb/users/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from users.views import signup, auth_token, UsersViewSet
+
+v1_router = DefaultRouter()
+
+v1_router.register(r'users', UsersViewSet, basename='users')
+
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+    path('v1/auth/signup/', signup),
+    path('v1/auth/token/', auth_token),
+
+]
